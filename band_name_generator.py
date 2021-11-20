37bffec609d342f034a07e1472cff92a3e2e990e
JustinDiter <48914465+JustinDiter@users.noreply.github.com>
2021-11-20 14:47:27 +0100

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print("Hi ! This is the first project in '100 Days of Code', a band name generator.")
+# Asks the name of the city the user grew up in
+city_name = input("Which city did you grow up in ?\n")
+
+# Asks the name of the user's first pet
+pet_name = input("What was the name of your first pet ?\n")
+
+# Puts both input together for the band name
+print("Your band name should be " + city_name + " " + pet_name + ".")
