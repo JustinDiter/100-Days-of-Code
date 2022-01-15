21488fc03e756dcb76ffc575f58074c23cab4cb7
JustinDiter <48914465+JustinDiter@users.noreply.github.com>
2022-01-15 10:34:48 +0100

    Update main.py

--- Day_26/NATO_Phonetic_Translator/main.py
+++ Day_26/NATO_Phonetic_Translator/main.py
@@ -2,15 +2,16 @@ import pandas as pd
 
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 
+# Creates a dictionary of the letters of the alphabet as the keys and the phonetic alphabet equivalent using comprehension
 for (index, row) in df.iterrows():
     Phonetic_Alphabet = {str(row.letter):str(row.code) for (index, row) in df.iterrows()}
 
-print(Phonetic_Alphabet)
+# For testing purposes
+# print(Phonetic_Alphabet) 
 
+# The user inputs a word and a list of the word translated to the NATO Phonetic Alphabet is created using comprehension and 
 user_word = input("Input a word to translate in the NATO phonetic alphabet: ")
 user_word_translated = [Phonetic_Alphabet[c.upper()] for c in user_word]
 
 print(user_word_translated)
 
-# translated_list = [value for (key, value) in Phonetic_Alphabet.items() for letter in user_word_formatted if key == letter]
-# print(translated_list)
